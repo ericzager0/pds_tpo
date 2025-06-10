cadc330b9197a03241efba995d9998961d1ead79
Eric Žager <ericzager0@gmail.com>
2025-06-10 18:49:19 -0300

    Se agrego la query geoespacial

--- unomas/src/main/java/com/pdstpo/unomas/repositories/IUserRepository.java
+++ unomas/src/main/java/com/pdstpo/unomas/repositories/IUserRepository.java
@@ -2,7 +2,14 @@ package com.pdstpo.unomas.repositories;
 
 import com.pdstpo.unomas.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IUserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
+
+    @Query(value = "SELECT * FROM users WHERE ST_DWithin(location::geography, ST_SetSRID(ST_MakePoint(:lng, :lat), 4326)::geography, :distance)", nativeQuery = true)
+    List<User> findUsersNearLocation(@Param("lng") double lng, @Param("lat") double lat, @Param("distance") double distance);
 }
