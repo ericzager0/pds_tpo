cd7bf48b7cf9fbf99d5bb02e9b74baccf074341d
Eric Žager <ericzager0@gmail.com>
2025-06-10 19:01:48 -0300

    Se implementaron los estados de Cancelado y Confirmado

--- unomas/src/main/java/com/pdstpo/unomas/model/Confirmado.java
+++ unomas/src/main/java/com/pdstpo/unomas/model/Confirmado.java
@@ -6,21 +6,21 @@ public class Confirmado extends State {
 
     @Override
     public void addPlayer(User user) {
-
+        throw new IllegalStateException("No es posible agregar un jugador a un partido que finalizó.");
     }
 
     @Override
     public void removePlayer(User user) {
-
+        throw new IllegalStateException("No es posible eliminar un jugador de un partido que finalizó.");
     }
 
     @Override
     public void confirm(User requestingUser) {
-
+        throw new IllegalStateException("No es posible confirmar un partido que finalizó.");
     }
 
     @Override
     public void cancel(User requestingUser) {
-
+        throw new IllegalStateException("No es posible cancelar un partido que finalizó.");
     }
 }
